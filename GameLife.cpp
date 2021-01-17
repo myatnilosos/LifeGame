1627a2c619ba86625e15f25468e977f2c5438585
myatnilosos <76513052+myatnilosos@users.noreply.github.com>
2021-01-17 20:23:38 +0300

    added while in main

--- GameLife.cpp
+++ GameLife.cpp
@@ -10,24 +10,42 @@ using namespace std;
 void initArray(char life[ROWS][COLS]);
 void showArray(char life[ROWS][COLS]);
 void copyArray(char life[ROWS][COLS], char copy[ROWS][COLS]);
-int countAliveCells(char life[ROWS][COLS]);
+int countAliveCells(char life[ROWS][COLS], int i, int j);
 
 int main()
 {
 	char life[ROWS][COLS] = {0}, copy[ROWS][COLS] = {0};
-	
+	bool flag = true;
+		
 	initArray(life);
 	showArray(life);
-	copyArray(life, copy);
 	
-	for(int i = 0; i < ROWS; i++){
-		for(int j = 0; j < COLS; j++){
-			if(life[i][j] == DEAD and countAliveCells(life) == 3){
-				
+	while(flag){
+		flag = false;
+		
+		copyArray(life, copy);
+	
+		cout << endl;
+
+		for(int i = 0; i < ROWS; i++){
+			for(int j = 0; j < COLS; j++){
+				if(copy[i][j] == DEAD and countAliveCells(copy, i, j) == 3){
+					life[i][j] = ALIVE;
+				}
+				if((copy[i][j] == ALIVE and countAliveCells(copy, i, j) < 2) or (copy[i][j] == ALIVE and countAliveCells(copy, i, j) > 3)){
+					life[i][j] = DEAD;
+				}
 			}
 		}
+		for(int i = 0; i < ROWS; i++){
+			for(int j = 0; j < COLS; j++){
+				if(copy[i][j] != life[i][j]){
+					flag = true;
+				}
+			}
+		}
+		showArray(life);
 	}
-	
     return 0;
 }
 
@@ -56,6 +74,7 @@ void showArray(char life[ROWS][COLS])
 	    }
 	    cout << endl;
 	}	
+	cout << "-------------------" << endl;
 }
 
 void copyArray(char life[ROWS][COLS], char copy[ROWS][COLS])
@@ -66,175 +85,180 @@ void copyArray(char life[ROWS][COLS], char copy[ROWS][COLS])
 	}
 }
 
-int countAliveCells(char life[ROWS][COLS])
+int countAliveCells(char life[ROWS][COLS], int i, int j)
 {
 	int counter = 0;
 	
-	for (int i = 0; i < ROWS; i++){
-    	for(int j = 0; j < COLS; j++){
-    		if((i > 0 and j > 0) and (i < (ROWS - 1) and j < (COLS - 1))){
-	    		if(life[i-1][j] == ALIVE)
-    				counter++;
-    			if(life[i-1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i][j+1] == ALIVE)
-    				counter++;
-    			if(life[i+1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i+1][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][j-1] == ALIVE)
-    				counter++;
-	    		if(life[i][j-1] == ALIVE)
-    				counter++;
-    			if(life[i-1][j-1] == ALIVE)
-    				counter++;
-    		}
-    		else if(i == 0 and (j > 0 and j < (COLS - 1))){
-	    		if(life[ROWS-1][j] == ALIVE)
-    				counter++;
-    			if(life[ROWS-1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i][j+1] == ALIVE)
-    				counter++;
-    			if(life[i+1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i+1][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][j-1] == ALIVE)
-    				counter++;
-	    		if(life[i][j-1] == ALIVE)
-    				counter++;
-    			if(life[ROWS-1][j-1] == ALIVE)
-    				counter++;
-    		}
-    		else if(i == (ROWS - 1) and (j > 0 and j < (COLS - 1))){
-	    		if(life[i-1][j] == ALIVE)
-    				counter++;
-    			if(life[i-1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i][j+1] == ALIVE)
-    				counter++;
-    			if(life[0][j+1] == ALIVE)
-    				counter++;
-	    		if(life[0][j] == ALIVE)
-    				counter++;
-    			if(life[0][j-1] == ALIVE)
-    				counter++;
-	    		if(life[i][j-1] == ALIVE)
-    				counter++;
-    			if(life[i-1][j-1] == ALIVE)
-    				counter++;
-    		}
-    		else if((i > 0 and i < (ROWS - 1)) and j == 0){
-	    		if(life[i-1][j] == ALIVE)
-    				counter++;
-    			if(life[i-1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i][j+1] == ALIVE)
-    				counter++;
-    			if(life[i+1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i+1][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][COLS - 1] == ALIVE)
-    				counter++;
-	    		if(life[i][COLS - 1] == ALIVE)
-    				counter++;
-    			if(life[i-1][COLS - 1] == ALIVE)
-    				counter++;
-    		}
-    		else if((i > 0 and i < (ROWS - 1)) and j == (COLS - 1)){
-    			if(life[i-1][j] == ALIVE)
-    				counter++;
-    			if(life[i-1][0] == ALIVE)
-    				counter++;
-	    		if(life[i][0] == ALIVE)
-    				counter++;
-    			if(life[i+1][0] == ALIVE)
-    				counter++;
-	    		if(life[i+1][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][j-1] == ALIVE)
-    				counter++;
-	    		if(life[i][j-1] == ALIVE)
-    				counter++;
-    			if(life[i-1][j-1] == ALIVE)
-    				counter++;
-    		}
-    		else if(i == 0 and j == 0){
-    			if(life[ROWS-1][j] == ALIVE)
-    				counter++;
-    			if(life[ROWS-1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i][j+1] == ALIVE)
-    				counter++;
-    			if(life[i+1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i+1][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][COLS-1] == ALIVE)
-    				counter++;
-	    		if(life[i][COLS-1] == ALIVE)
-    				counter++;
-    			if(life[ROWS-1][COLS-1] == ALIVE)
-    				counter++;
-    		}
-    		else if(i == 0 and j == (COLS - 1)){
-    			if(life[ROWS-1][j] == ALIVE)
-    				counter++;
-    			if(life[ROWS-1][0] == ALIVE)
-    				counter++;
-	    		if(life[i][0] == ALIVE)
-    				counter++;
-    			if(life[i+1][0] == ALIVE)
-    				counter++;
-	    		if(life[i+1][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][COLS-1] == ALIVE)
-    				counter++;
-	    		if(life[i][COLS-1] == ALIVE)
-    				counter++;
-    			if(life[ROWS-1][j-1] == ALIVE)
-    				counter++;
-    		}
-    		else if(i == (ROWS - 1) and j == (COLS - 1)){
-	    		if(life[i-1][j] == ALIVE)
-    				counter++;
-    			if(life[i-1][0] == ALIVE)
-    				counter++;
-	    		if(life[i][0] == ALIVE)
-    				counter++;
-    			if(life[0][0] == ALIVE)
-    				counter++;
-	    		if(life[0][j] == ALIVE)
-    				counter++;
-    			if(life[0][j-1] == ALIVE)
-    				counter++;
-	    		if(life[i][j-1] == ALIVE)
-    				counter++;
-    			if(life[i-1][j-1] == ALIVE)
-    				counter++;
-    		}
-    		else if(i == (ROWS - 1) and j == 0){
-	    		if(life[i-1][j] == ALIVE)
-    				counter++;
-    			if(life[i-1][j+1] == ALIVE)
-    				counter++;
-	    		if(life[i][j+1] == ALIVE)
-    				counter++;
-    			if(life[0][j+1] == ALIVE)
-    				counter++;
-	    		if(life[0][j] == ALIVE)
-    				counter++;
-    			if(life[i+1][COLS-1] == ALIVE)
-    				counter++;
-	    		if(life[i][COLS-1] == ALIVE)
-    				counter++;
-    			if(life[i-1][COLS-1] == ALIVE)
-    				counter++;
-    		}
-    	}
+	if((i > 0 and j > 0) and (i < (ROWS - 1) and j < (COLS - 1))){
+		counter = 0;
+   		if(life[i-1][j] == ALIVE)
+			counter++;
+		if(life[i-1][j+1] == ALIVE)
+			counter++;
+   		if(life[i][j+1] == ALIVE)
+			counter++;
+		if(life[i+1][j+1] == ALIVE)
+			counter++;
+   		if(life[i+1][j] == ALIVE)
+			counter++;
+		if(life[i+1][j-1] == ALIVE)
+			counter++;
+   		if(life[i][j-1] == ALIVE)
+			counter++;
+		if(life[i-1][j-1] == ALIVE)
+			counter++;
 	}
-	return counter;
+	else if(i == 0 and (j > 0 and j < (COLS - 1))){
+		counter = 0;
+   		if(life[ROWS-1][j] == ALIVE)
+			counter++;
+    	if(life[ROWS-1][j+1] == ALIVE)
+    		counter++;
+	    if(life[i][j+1] == ALIVE)
+    		counter++;
+    	if(life[i+1][j+1] == ALIVE)
+    		counter++;
+	    if(life[i+1][j] == ALIVE)
+    		counter++;
+    	if(life[i+1][j-1] == ALIVE)
+    		counter++;
+	    if(life[i][j-1] == ALIVE)
+    		counter++;
+    	if(life[ROWS-1][j-1] == ALIVE)
+    		counter++;
+   	}
+    else if(i == (ROWS - 1) and (j > 0 and j < (COLS - 1))){
+    	counter = 0;
+		if(life[i-1][j] == ALIVE)
+    		counter++;
+    	if(life[i-1][j+1] == ALIVE)
+    		counter++;
+		if(life[i][j+1] == ALIVE)
+    		counter++;
+    	if(life[0][j+1] == ALIVE)
+    		counter++;
+		if(life[0][j] == ALIVE)
+    		counter++;
+    	if(life[0][j-1] == ALIVE)
+    		counter++;
+		if(life[i][j-1] == ALIVE)
+    		counter++;
+    	if(life[i-1][j-1] == ALIVE)
+    		counter++;
+    }
+    else if((i > 0 and i < (ROWS - 1)) and j == 0){
+    	counter = 0;
+		if(life[i-1][j] == ALIVE)
+    		counter++;
+    	if(life[i-1][j+1] == ALIVE)
+    		counter++;
+		if(life[i][j+1] == ALIVE)
+    		counter++;
+    	if(life[i+1][j+1] == ALIVE)
+    		counter++;
+		if(life[i+1][j] == ALIVE)
+    		counter++;
+    	if(life[i+1][COLS - 1] == ALIVE)
+    		counter++;
+		if(life[i][COLS - 1] == ALIVE)
+    		counter++;
+    	if(life[i-1][COLS - 1] == ALIVE)
+    		counter++;
+    }
+    else if((i > 0 and i < (ROWS - 1)) and j == (COLS - 1)){
+    	counter = 0;
+    	if(life[i-1][j] == ALIVE)
+    		counter++;
+    	if(life[i-1][0] == ALIVE)
+    		counter++;
+		if(life[i][0] == ALIVE)
+    		counter++;
+    	if(life[i+1][0] == ALIVE)
+    		counter++;
+		if(life[i+1][j] == ALIVE)
+    		counter++;
+    	if(life[i+1][j-1] == ALIVE)
+    		counter++;
+		if(life[i][j-1] == ALIVE)
+    		counter++;
+    	if(life[i-1][j-1] == ALIVE)
+    		counter++;
+    }
+    else if(i == 0 and j == 0){
+    	counter = 0;
+    	if(life[ROWS-1][j] == ALIVE)
+    		counter++;
+    	if(life[ROWS-1][j+1] == ALIVE)
+    		counter++;
+		if(life[i][j+1] == ALIVE)
+    		counter++;
+    	if(life[i+1][j+1] == ALIVE)
+    		counter++;
+		if(life[i+1][j] == ALIVE)
+    		counter++;
+    	if(life[i+1][COLS-1] == ALIVE)
+    		counter++;
+		if(life[i][COLS-1] == ALIVE)
+    		counter++;
+    	if(life[ROWS-1][COLS-1] == ALIVE)
+    		counter++;
+    }
+    else if(i == 0 and j == (COLS - 1)){
+    	counter = 0;
+    	if(life[ROWS-1][j] == ALIVE)
+    		counter++;
+    	if(life[ROWS-1][0] == ALIVE)
+    		counter++;
+		if(life[i][0] == ALIVE)
+    		counter++;
+    	if(life[i+1][0] == ALIVE)
+    		counter++;
+		if(life[i+1][j] == ALIVE)
+    		counter++;
+    	if(life[i+1][COLS-1] == ALIVE)
+    		counter++;
+		if(life[i][COLS-1] == ALIVE)
+    		counter++;
+    	if(life[ROWS-1][j-1] == ALIVE)
+    		counter++;
+    }
+    else if(i == (ROWS - 1) and j == (COLS - 1)){
+    	counter = 0;
+		if(life[i-1][j] == ALIVE)
+    		counter++;
+    	if(life[i-1][0] == ALIVE)
+    		counter++;
+		if(life[i][0] == ALIVE)
+    		counter++;
+    	if(life[0][0] == ALIVE)
+    		counter++;
+		if(life[0][j] == ALIVE)
+    		counter++;
+    	if(life[0][j-1] == ALIVE)
+    		counter++;
+		if(life[i][j-1] == ALIVE)
+    		counter++;
+    	if(life[i-1][j-1] == ALIVE)
+    		counter++;
+    }
+    else if(i == (ROWS - 1) and j == 0){
+    	counter = 0;
+		if(life[i-1][j] == ALIVE)
+    		counter++;
+    	if(life[i-1][j+1] == ALIVE)
+    		counter++;
+		if(life[i][j+1] == ALIVE)
+    		counter++;
+    	if(life[0][j+1] == ALIVE)
+    		counter++;
+		if(life[0][j] == ALIVE)
+    		counter++;
+    	if(life[i+1][COLS-1] == ALIVE)
+    		counter++;
+		if(life[i][COLS-1] == ALIVE)
+    		counter++;
+    	if(life[i-1][COLS-1] == ALIVE)
+    		counter++;
+    }
+    return counter;
 }
